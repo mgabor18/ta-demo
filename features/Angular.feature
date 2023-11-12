f7911d5b2266bf72a9ee72a7ee8f449aa6b7c181
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2023-11-12 16:47:22 +0100

    Create example solution

--- features/Angular.feature
+++ features/Angular.feature
@@ -4,7 +4,18 @@ Feature: Angular TA demo
   So that I can show a nice demo, how automation basics work
 
   Scenario: The user should have access to the Angular main page
+  Given the user opens the Angular page
+  Then the URL should be correct
+  And the page's title should be "Angular"
 
   Scenario: The Angular page loads properly
+  Given the user opens the Angular page
+  Then the page's title should be "Angular"
+  And the "The web development framework for building the future" is visible
+  And the "The web development framework for building the future" text is correct
 
   Scenario: The user is able to open the Docs page
+  Given the user opens the Angular page
+  When the user clicks the Docs button in the header
+  Then the Docs page should be opened
+  And the introduction text should be visible
--- step_definitions/index.js
+++ step_definitions/index.js
@@ -1,1 +1,51 @@
 const { Given, When, Then } = require('cucumber');
+
+Given('the user opens the Angular page', function () {
+  return driver.get('https://angular.io/');
+});
+
+Then('the URL should be correct', function () {
+  const URL = driver.getCurrentUrl();
+
+  return expect(URL).to.eventually.equal('https://angular.io/');
+});
+
+Then("the page's title should be {string}", function (title) {
+  const pageTitle = driver.getTitle();
+
+  return expect(pageTitle).to.eventually.equal(title);
+});
+
+Then('the {string} is visible', function (string) {
+  const selector = '#promo-0-title';
+  const isTextVisible = driver.findElement(by.css(selector)).isDisplayed();
+
+  return expect(isTextVisible).to.eventually.be.true;
+});
+
+Then('the {string} text is correct', function (text) {
+  const selector = '#promo-0-title';
+  const pageText = driver.findElement(by.css(selector)).getText();
+
+  return expect(pageText).to.eventually.be.equal(text);
+});
+
+When('the user clicks the Docs button in the header', function () {
+  const selector = '.nav-link[href=docs]';
+
+  return driver.findElement(by.css(selector)).click();
+});
+
+Then('the Docs page should be opened', function () {
+  const url = driver.getCurrentUrl();
+
+  return expect(url).to.eventually.equal('https://angular.io/docs');
+});
+
+Then('the introduction text should be visible', async function () {
+  const selector = 'h1#introduction-to-the-angular-docs';
+  await driver.sleep(1000);
+  const isTextVisible = driver.findElement(by.css(selector)).isDisplayed();
+
+  return expect(isTextVisible).to.eventually.be.true;
+});
